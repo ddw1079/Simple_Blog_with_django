00399e7c01619cf5720af22c9f7aaafe8e64f4ef
KHS <ddw1079@naver.com>
2022-12-08 15:22:04 +0900

    Add Edit Post Feature

--- blog/urls.py
+++ blog/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('', views.main_page, name='main_page'),
     path('blog/', views.post_list, name='post_list'),
     path('blog/post/<int:pk>/', views.post_detail, name='post_detail'),
-    path('post/new/', views.post_new, name='post_new')
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit', views.post_edit, name='post_edit')
 ]
