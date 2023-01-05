1b9867e5eb410acb37d57e46d1fa480ca478ce5f
KHS <ddw1079@naver.com>
2023-01-05 09:52:48 +0900

    form에 HTML 클래스 적용 Rich Text Editor 인 Summernote 적용

--- blog/forms.py
+++ blog/forms.py
@@ -5,4 +5,18 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = { 'title', 'text'}
+        widgets = {
+            'title': forms.TextInput(attrs={
+                'class': "form-control",
+                'id': '',
+                'style': "",
+                'placeholder': "title"
+            }),
+            'text': forms.Textarea(attrs={
+                'class': "form-control",
+                'id': 'article_input',
+                'style': "",
+                "placeholder": "text"
+            }),
+        }
         
